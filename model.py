810ba6349c47adf413a633a9b5680b4ff9dd73e9
Koki Mitsunami <k.mitsunami@gmail.com>
2018-08-26 22:26:26 +0900

    update model.py and run 1_Preliminaries.ipynb

--- model.py
+++ model.py
@@ -23,10 +23,16 @@ class EncoderCNN(nn.Module):
 
 class DecoderRNN(nn.Module):
     def __init__(self, embed_size, hidden_size, vocab_size, num_layers=1):
-        pass
+        super(DecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
     
     def forward(self, features, captions):
-        pass
+        embeddings = self.embed(captions[:, :-1])
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        hiddens, _ = self.lstm(embeddings)
+        outputs = self.linear(hiddens)
         # outputs should be a PyTorch tensor with size [batch_size, captions.shape[1], vocab_size]
         # Your output should be designed such that outputs[i,j,k] contains the model's predicted score, indicating how likely the j-th token in the i-th caption in the batch is the k-th token in the vocabulary. 
         return outputs
